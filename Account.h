c7ef2e03d06dc02178ae4784a239d518657c43e1
jpandya <jpandya@superfinestudios.co>
2013-10-15 01:40:13 -0400

    Update Account.h

--- Account.h
+++ Account.h
@@ -12,6 +12,9 @@ class Account
 
 public:
   Account(char *fname, char *sname, int SIN, char *accounttype, int transactions, float balance);
+  char *getfirstname(void);
+  char *getsurame(void);
+  int getsocialinsurance(void);
   float DepositAmt(void);
   float WithdrawAmt(void);
   float getFinalBalance(void);
