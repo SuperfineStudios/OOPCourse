b35145ca9dda549bd049fe40aba6a4eb32f5e1d4
jpandya <jpandya@superfinestudios.co>
2013-10-15 01:30:13 -0400

    Update account.cpp

--- account.cpp
+++ account.cpp
@@ -1,3 +1,30 @@
 #include<string.h>
-#include"account.h"
+#include"Account.h"
 
+Account::Account(char *fname, char *sname, int SIN, char *accounttype, int transactions, float balance)
+{
+   strcpy(szFName, FirstName);
+   strcpy(szLName, LastName);
+   nIncome = Income;
+}
+
+char *Employee::getFName(void)
+{
+   return szFName;
+}
+
+char *Employee::getLName(void)
+{
+   return szLName;
+}
+
+float Account::DepositAmt(void);
+{
+
+}
+float Account::WithdrawAmt(void);
+{
+
+}
+float Account::getFinalBalance(void);
+void Account::PrintStatement(void);
